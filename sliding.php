4cf1e33d9b765fb857242a1b0b117d44ecae65ab
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 11:28:07 +0530

    Update sliding.php

--- sliding.php
+++ sliding.php
@@ -1,8 +1,8 @@
-<div class="bg-white py-6 font-mono">
-    <div id="overview" class="w-full h-full flex items-center justify-center bg-gray-100/80 select-none blur-sm mt-10">
-        <h2 class='text-center text-3xl md:text-4xl z-50 text-gray-500 animate-pulse font-semibold blur-sm'>Revealing Soon !!</h2>
-    </div>
-    <!-- <marquee>
+<!-- <div class="bg-white py-6 font-mono"> -->
+<div id="overview" class="w-full h-full flex items-center justify-center bg-gray-100/80 select-none blur-sm mt-10">
+    <h2 class='text-center text-[100px] z-50 text-gray-500 font-semibold blur-sm'>Revealing Soon !!</h2>
+</div>
+<!-- <marquee>
         <div>The best-selected papers presented at the conference will get the opportunity to be published in the Springer SCI Index Journal Special Issue "Optical and Quantum Electronics".</div>
     </marquee> -->
-</div>
+<!-- </div> -->

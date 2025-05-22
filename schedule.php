ad7025607235c759acab1279b5d05c00a1039de4
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 11:27:51 +0530

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -1,16 +1,15 @@
-<div id="schedule" class="h-8 bg-white w-full">
+<!-- <div id="schedule" class="h-8 bg-white w-full">
 
-</div>
+</div> -->
 
-<div class='w-full font-sans'>
-  <!-- <div class='bg-[#e9e9e 9] bg-white px-[15%]'>
+<!-- <div class='w-full font-sans'></div> -->
+<!-- <div class='bg-[#e9e9e 9] bg-white px-[15%]'>
       <div>
           <h1 class='text-[#c00000] pt-16 text-center text-3xl font-[600]'>SCHEDULE</h1>
           <div class='w-32 h-[3px] mx-auto mt-2 bg-[#365372] rounded-xl'></div>
         </div>
         <p class="text-center text-md md:text-xl pt-8">Will be declared 10 days prior to the conference.</p>
     </div> -->
-  <div id="overview" class="w-full h-full flex items-center justify-center bg-gray-100/80 select-none blur-sm mt-10">
-    <h2 class='text-center text-3xl md:text-4xl z-50 text-gray-500 font-semibold blur-sm animate-pulse'>Revealing Soon Schedule !!</h2>
-  </div>
+<div id="schedule" class="w-full h-full flex items-center justify-center bg-gray-100/80 select-none blur-sm mt-10">
+  <h2 class='text-center text-3xl md:text-4xl animate-pulse z-50 text-gray-500 font-semibold blur-sm'>Revealing Soon Schedule !!</h2>
 </div>

cc1600ed2d787367fe749f0e4dc9d33a9ff8667c
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 11:26:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://kit.fontawesome.com/4e315737a7.js" crossorigin="anonymous"></script>
-     <script src="https://cdn.tailwindcss.com"></script>
-    <title>ICAIA 2025</title>
-    <link rel="icon" href="./images/icaiaLogo.png" type="image/icon type">
-    <link rel="stylesheet" href="style4.css">
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=cabinet-grotesk@300&f[]=stardom@400&display=swap" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="slider.css"> -->
-    <style>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://kit.fontawesome.com/4e315737a7.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>ICAIA 2026</title>
+  <link rel="icon" href="./images/icaiaLogo.png" type="image/icon type">
+  <link rel="stylesheet" href="style4.css">
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link href="https://api.fontshare.com/v2/css?f[]=satoshi@700&f[]=cabinet-grotesk@300&f[]=stardom@400&display=swap" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="slider.css"> -->
+  <style>
     .pulse {
       animation: pulse 1s infinite;
     }
@@ -22,36 +23,39 @@
       0% {
         transform: scale(1);
       }
+
       50% {
         transform: scale(1.1);
       }
+
       100% {
         transform: scale(1);
       }
     }
   </style>
-  </head>
-  <body>
-    <?php include("./navbar.php")?>
-    <?php include("./header.php")?>
-    
-    <!-- <?php include("./sliding.php")?> -->
-    <?php include("./about.php")?>
-    <?php include("./keynote.php")?>
-    <?php include("./committee.php")?>
-    <?php include("./tracks.php")?>
-    <?php include("./sponsorship.php")?> 
-    <?php include("./submissions.php")?>
-    <!-- <?php include("./schedule.php")?> -->
-    <?php include("./registrations.php")?>
-    <?php include("./callforpapers.php")?>
-    <?php include("./dates.php")?>
-    <?php include("./institute.php")?>
-    <?php include("./video.php")?>
-   <?php include("./contact.php")?>
-    <?php include("./footer.php")?>
-  </body>
-  <script src="
+</head>
+
+<body>
+  <?php include("./navbar.php") ?>
+  <?php include("./header.php") ?>
+
+  <!-- <?php include("./sliding.php") ?> -->
+  <?php include("./about.php") ?>
+  <?php include("./keynote.php") ?>
+  <?php include("./committee.php") ?>
+  <?php include("./tracks.php") ?>
+  <?php include("./sponsorship.php") ?>
+  <?php include("./submissions.php") ?>
+  <!-- <?php include("./schedule.php") ?> -->
+  <?php include("./registrations.php") ?>
+  <?php include("./callforpapers.php") ?>
+  <?php include("./dates.php") ?>
+  <?php include("./institute.php") ?>
+  <?php include("./video.php") ?>
+  <?php include("./contact.php") ?>
+  <?php include("./footer.php") ?>
+</body>
+<script src="
 https://cdn.jsdelivr.net/npm/tiny-slider@2.9.4/dist/min/tiny-slider.min.js
 "></script>
 <link href="
@@ -61,10 +65,11 @@ https://cdn.jsdelivr.net/npm/tiny-slider@2.9.4/dist/tiny-slider.min.css
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=EB+Garamond&family=Mada:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <script src="./scripts/hamburger.js"></script>
-  <script src="./scripts/scrollNav.js"></script>
-  <script src="./scripts/committees.js"></script>
- <script src="./scripts/committeesDropDown.js"></script>
- <!-- <script src="./scripts/highlight.js"></script> -->
- <script src="./scripts/sponsorship.js"></script>
+<script src="./scripts/hamburger.js"></script>
+<script src="./scripts/scrollNav.js"></script>
+<script src="./scripts/committees.js"></script>
+<script src="./scripts/committeesDropDown.js"></script>
+<!-- <script src="./scripts/highlight.js"></script> -->
+<script src="./scripts/sponsorship.js"></script>
+
 </html>

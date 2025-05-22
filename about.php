6b9fc273809ab1cb151790ea64627ad842e854f4
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 10:53:02 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,35 +1,49 @@
-<div id="overview" class='w-full  text-white'>
+<div id="overview" class="w-full h-full flex items-center justify-center select-none bg-gray-100/80 blur-sm">
+  <h2 class='text-center text-3xl md:text-4xl animate-pulse z-50 text-gray-500 font-semibold blur-sm'>✨ Revealing Soon !!</h2>
+
+  <!-- 
   <div class='bg-blue-400'>
     <div class='mt-4'>
       <div class="w-full py-6">
         <marquee class="text-xl">
           <div class="flex w-[250%]">
-            <div><img src="./images/new.gif" alt="123" class="mt-2"></div><a href="#submissionsText"
-              class="ml-2 text-[#FF0000 ] text-white text-lg mr-2"> Few good quality accepted and presented papers will be selected, and after further reviewing will be published in the following SCI/Scopus indexed journals :</a>
+            <div><img src="./images/new.gif" alt="123" class="mt-2"></div>
+            <a href="#submissionsText" class="ml-2 text-[#FF0000 ] text-white text-lg mr-2">
+              Few good quality accepted and presented papers will be selected, and after further reviewing will be published in the following SCI/Scopus indexed journals :
+            </a>
           </div>
           <div class="flex w-[500%]">
-            <div><img src="./images/new.gif" alt="123" class="mt-2"></div><a href="#submissionsText"
-              class="ml-2 text-[#FF0000 ] text-white text-lg "> MAPAN Journal of Metrology Society Of India(SCI)</a>
+            <div><img src="./images/new.gif" alt="123" class="mt-2"></div>
+            <a href="#submissionsText" class="ml-2 text-[#FF0000 ] text-white text-lg">
+              MAPAN Journal of Metrology Society Of India (SCI)
+            </a>
           </div>
           <div class="flex w-[500%]">
-            <div><img src="./images/new.gif" alt="123" class="mt-2"></div><a href="#"
-              class="ml-2 text-[#FF0000 ] text-white text-lg "> Proceedings on Engineering Sciences(Scopus)</a>
+            <div><img src="./images/new.gif" alt="123" class="mt-2"></div>
+            <a href="#" class="ml-2 text-[#FF0000 ] text-white text-lg">
+              Proceedings on Engineering Sciences (Scopus)
+            </a>
           </div>
           <div class="flex w-[250%]">
-            <div><img src="./images/new.gif" alt="123" class="mt-2"></div><a href="#"
-              class="ml-2 text-[#FF0000 ] text-white text-lg "> Intelligent Decision Technologies(Scopus)</a>
+            <div><img src="./images/new.gif" alt="123" class="mt-2"></div>
+            <a href="#" class="ml-2 text-[#FF0000 ] text-white text-lg">
+              Intelligent Decision Technologies (Scopus)
+            </a>
           </div>
         </marquee>
       </div>
-      <div class=" px-[8%] md:px-[15%]">
+
+      <div class="px-[8%] md:px-[15%]">
         <h1 class='text-[#c00000] pt-6 text-center text-3xl font-[600]'>CONFERENCE OVERVIEW</h1>
         <div class='w-72 h-[3px] mx-auto mt-2 bg-[#365372] rounded-xl'></div>
       </div>
+
       <p class='text-md md:text-xl px-[15%] mt-8 pb-28 text-justify'>
-        Maharaja Surajmal Institute of Technology (MSIT) in New Delhi, India, is organizing the 3rd International Conference ICAIA-2025 on 26-27 March 2025. The
+        Maharaja Surajmal Institute of Technology (MSIT) in New Delhi, India, is organizing the 3rd International Conference ICAIA-2025 on 26–27 March 2025. The
         conference aims to provide a platform for researchers and practitioners from both academia as well as industry
         to meet and share cutting-edge development in the field of Artificial Intelligence and its Applications.
       </p>
     </div>
-  </div>
+  </div> 
+  -->
 </div>

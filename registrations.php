b610284026fc421b368e86b3a2a7fd80b058f0ce
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 10:56:13 +0530

    Update registrations.php

--- registrations.php
+++ registrations.php
@@ -1,16 +1,17 @@
-<div id="registrations"  class="h-8 bg-white w-full">
+<div id="registrations" class="h-8 bg-white w-full">
 </div>
 
-<div class='w-full text-justify'>
-      <div class='bg-white px-[5%] md:px-[15%]'>
+<div id="overview" class="w-full h-full flex items-center justify-center bg-gray-100/80 select-none blur-sm mt-10">
+    <h2 class='text-center text-4xl md:text-3xl animate-pulse z-50 text-gray-500 font-semibold blur-sm'>Revealing Soon Registerations !!</h2>
+    <!-- <div class='bg-white px-[5%] md:px-[15%]'>
       <div>
         <h1 class='text-[#c00000] pt-16 text-center text-3xl font-[600]'>REGISTRATIONS</h1>
         <div class='w-48 h-[3px] mx-auto mt-2 bg-[#365372] rounded-xl'></div>
-        <div class="h-12 bg-white w-full"></div>
-        <!-- <div class="mt-20 pb-[50px] pt-16 text-center text-3xl font-[450]">
+        <div class="h-12 bg-white w-full"></div> -->
+    <!-- <div class="mt-20 pb-[50px] pt-16 text-center text-3xl font-[450]">
             COMING SOON
         </div> -->
-      </div class="mx-auto">
+    <!-- </div class="mx-auto">
       <h2 class=" text-md md:text-lg "><strong>Author/Co-Author Registration Fee Includes: Participation in the technical program, Participation certificate to the presenter</strong></h2>
       <br>
       <p>After acceptance of the paper, the registration process begins. It is mandatory for at least one author of an accepted paper to register in order for the paper to appear in the proceedings and included in the Technical Program. </p>
@@ -61,15 +62,15 @@
             <div class="w-1/3 text-center">500 Per Page</div>
             <div class='w-1 h-auto border-r-2 border-white'></div>
             <div class="w-1/3 text-center">15 Per Page</div>
-        </div>
-        <!-- <div class="flex   text-md md:text-lg md:text-xl py-3">
+        </div> -->
+    <!-- <div class="flex   text-md md:text-lg md:text-xl py-3">
             <div class="w-1/3 text-center">Journal</div>
             <div class='w-1 h-auto border-r-2 border-[#e9e9e9]'></div>
             <div class="w-1/3 text-center">18,000</div>
             <div class='w-1 h-auto border-r-2 border-[#e9e9e9]'></div>
             <div class="w-1/3 text-center">450 </div>
         </div> -->
-        <br>
+    <!-- <br>
         <br>
         <br>
         <h2 class="pb-6 text-md md:text-lg "><strong>Author/Co-Author Registration Fee Includes:</strong><br>
@@ -86,5 +87,5 @@
         <p class=" text-md md:text-lg "><li>A candidate once registered and paid the registration fee is not allowed to cancel the registration. The registration fee for the conference once paid is not refundable.</li></p>
         <br>
         <p class=" text-md md:text-lg "><li>The conference is non-residential and delegates are kindly requested to make their own arrangement for accommodation. If you have any query, please drop a mail to us. </li></p>
-      </div>
+      </div> -->
 </div>

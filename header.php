1c57ad6dbfd9d51453cd6938d9524e719561d2d8
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 11:45:09 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
-<!-- 
 <div class='z-[-1] pt-[90px]'>
   <span class='bg-[#fec913] text-white font-bold text-center py-2 px-3 rounded-b-xl shadow-lg shadow-black-500/50 
               inline-block transform transition-all duration-300 ease-in-out 
@@ -11,41 +10,41 @@
     <img class="w-[150px] mx-auto " src="./images/icaiaLogo.png" alt="">
   </div>
   <div class='mx-[5%] md:mx-[15%] py-6 rounded-xl shadow border-2 border-white' style='background-color: rgba(255, 255, 255, 0.6);'>
-  <div class='text-center text-white text-xl md:text-2xl lg:text-3xl font-[700] mx-auto'>
+    <div class='text-center text-white text-xl md:text-2xl lg:text-3xl font-[700] mx-auto'>
 
-    <div class="flex items-center justify-between w-[95%] mx-auto">
-      <img class='z-10 w-[90px] md:w-[110px] lg:w-[130px] px-2 mt-3 transition-transform duration-300 hover:scale-110' src="./images/ITDATA.jpeg.jpg" alt="Left Logo" />
+      <div class="flex items-center justify-between w-[95%] mx-auto">
+        <img class='z-10 w-[90px] md:w-[110px] lg:w-[130px] px-2 mt-3 transition-transform duration-300 hover:scale-110' src="./images/ITDATA.jpeg.jpg" alt="Left Logo" />
 
-      <div class="flex flex-col items-center">
-        <img class='z-10 w-[100px] md:w-[120px] lg:w-[140px] mb-2 transition-transform duration-300 hover:scale-110' src="./images/springer-logo.svg" alt="Center Logo" />
-        <h1 class='z-10 md:tracking-widest tracking-[1px] text-blue-800'>3<sup>rd</sup> International Conference on</h1>
-        <h1 class="md:tracking-[2px] tracking-[1px] text-blue-800">Artificial Intelligence and Applications (ICAIA 2025)</h1>
+        <div class="flex flex-col items-center">
+          <img class='z-10 w-[100px] md:w-[120px] lg:w-[140px] mb-2 transition-transform duration-300 hover:scale-110' src="./images/springer-logo.svg" alt="Center Logo" />
+          <h1 class='z-10 md:tracking-widest tracking-[1px] text-blue-800'>4<sup>th</sup> International Conference on</h1>
+          <h1 class="md:tracking-[2px] tracking-[1px] text-blue-800">Artificial Intelligence and Applications (ICAIA 2026)</h1>
+        </div>
+
+        <img class='z-10 w-[90px] md:w-[110px] lg:w-[130px] px-2 mt-3 transition-transform duration-300 hover:scale-110' src="./images/UNMSM.jpeg.jpg" alt="Right Logo" />
       </div>
 
-      <img class='z-10 w-[90px] md:w-[110px] lg:w-[130px] px-2 mt-3 transition-transform duration-300 hover:scale-110' src="./images/UNMSM.jpeg.jpg" alt="Right Logo" />
+      <h1 class='tracking-[1px] md:tracking-[2.5px] text-[#D3B683] text-black mt-4 font-semibold text-2xl sm:text-xl md:text-2xl lg:text-3xl'>
+        Maharaja Surajmal Institute of Technology
+      </h1>
+
+      <h1 class='mt-4 tracking-[1.3px] md:tracking-[3px] text-[#D3B683] text-black text-2xl'>In collaboration with </h1>
+      <h1 class='tracking-[1px] md:tracking-[2.5px] text-[#D3B683] text-black mt-2 mx-3 md:mx-0'>
+        Universidad Nacional Mayor de San Marcos (UNMSM) and the research group ITDATA, Peru & Springer
+      </h1>
+
     </div>
-
-    <h1 class='tracking-[1px] md:tracking-[2.5px] text-[#D3B683] text-black mt-4 font-semibold text-2xl sm:text-xl md:text-2xl lg:text-3xl'>
-      Maharaja Surajmal Institute of Technology
-    </h1>
-
-    <h1 class='mt-4 tracking-[1.3px] md:tracking-[3px] text-[#D3B683] text-black text-2xl'>In collaboration with </h1>
-    <h1 class='tracking-[1px] md:tracking-[2.5px] text-[#D3B683] text-black mt-2 mx-3 md:mx-0'>
-      Universidad Nacional Mayor de San Marcos (UNMSM) and the research group ITDATA, Peru & Springer
-    </h1>
-
   </div>
-</div>
 
-  <div class='text-center text-2xl font-[700] mt-4'>
+  <!-- <div class='text-center text-2xl font-[700] mt-4'>
     <h1 class='text-[#fcb810 ] text-white text-3xl z-10 tracking-widest mt-3 font-bold bg-gradient-to-r from-slate-50 to-zinc-300 bg-clip-text text-transparent text-border event-date text-2xl sm:text-1xl md:text-2xl lg:text-3xl'> 26-27 March 2025</h1>
-  </div>
+  </div> -->
 </div>
--->
 
-<!-- Placeholder Section -->
+
+<!-- Placeholder Section
 <div class="text-center py-16">
   <h1 class="text-3xl md:text-4xl font-bold text-gray-700 blur-sm select-none animate-pulse">
     ✨ Revealing Soon !!
   </h1>
-</div>
+</div> -->

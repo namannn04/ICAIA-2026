4e09a971418d447a4b6aa7e3c6e5ea0d5b364f7c
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 11:01:17 +0530

    Update video.php

--- video.php
+++ video.php
@@ -1,9 +1,9 @@
 <div class='bg-white'>
-    <h1 class='text-[#c00000] pt-[50px] bg-white text-center text-3xl font-[600]'>Previous Conferences</h1>
-    <div class='w-32 h-[3px] mx-auto mt-2 bg-[#365372] rounded-xl'></div>
+    <!-- <h1 class='text-[#c00000] pt-[50px] bg-white text-center text-3xl font-[600]'>Previous Conferences</h1>
+    <div class='w-32 h-[3px] mx-auto mt-2 bg-[#365372] rounded-xl'></div> -->
 </div>
-<div id='video' class="bg-white flex items-center pt-[50px] justify-evenly h-[700px]">
-    <div class="mt-6">
+<!-- <div id='video' class="bg-white flex items-center pt-[50px] justify-evenly h-[700px]">
+     <div class="mt-6">
         <h1 class='text-center text-2xl font-[450] mt-4 font-bold text-orange-500'>ICAIA 2020</h1>
         <video class="h-[689px] object-cover mt-3" autoplay muted loop controls>
             <source src="assets/video.mp4" type="video/mp4">
@@ -19,4 +19,16 @@
             Your browser does not support the video tag.
         </video>
     </div>
+    <div class="mt-6">
+        <h1 class='text-center text-2xl font-[450] mt-4 font-bold text-orange-500'>ICAIA 2025</h1>
+        <video class="h-[689px] w-[411px] object-cover mt-3" autoplay muted loop controls>
+            <source src="assets/video2.mp4" type="video/mp4">
+            <source src="assets/video2.mp4" type="video/ogg">
+            Your browser does not support the video tag.
+        </video>
+    </div> -->
+<!-- </div> -->
+
+<div id="overview" class="w-full h-full flex items-center justify-center bg-gray-100/80 select-none blur-sm mt-10">
+    <h2 class='text-center text-[100px] z-50 text-gray-500 font-semibold blur-sm animate-pulse'>Revealing Soon !!</h2>
 </div>

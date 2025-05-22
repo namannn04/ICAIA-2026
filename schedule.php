4b193618ab724c8e2b7f7691dee03549dee439e5
Nevin-Bali100 <146621784+Nevin100@users.noreply.github.com>
2025-05-22 10:56:54 +0530

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -11,6 +11,6 @@
         <p class="text-center text-md md:text-xl pt-8">Will be declared 10 days prior to the conference.</p>
     </div> -->
   <div id="overview" class="w-full h-full flex items-center justify-center bg-gray-100/80 select-none blur-sm mt-10">
-    <h2 class='text-center text-[100px] z-50 text-gray-500 font-semibold blur-sm'>Revealing Soon Schedule !!</h2>
+    <h2 class='text-center text-3xl md:text-4xl z-50 text-gray-500 font-semibold blur-sm animate-pulse'>Revealing Soon Schedule !!</h2>
   </div>
 </div>

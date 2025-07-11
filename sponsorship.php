bffb0c0b070797abb0a2c94aa342f4c39bc7cd22
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-07-11 10:39:50 +0530

    Completing Code

--- sponsorship.php
+++ sponsorship.php
@@ -1,27 +1,26 @@
 <div class="h-8 bg-white w-full font-sans">
 
 </div>
-<div id="sponsor" class="w-full h-full flex select-none items-center justify-center bg-gray-100/80 blur-sm mt-10">
-    <h2 class='text-center text-[100px] z-50 blur-sm text-gray-500 font-semibold'>Revealing Soon Sponsorships !!</h2>
-    <!-- <div class="bg-blue-100 px-8 py-16 md:px-16">
+<div class="w-full" id="sponsor">
+    <div class="bg-blue-100 px-8 py-16 md:px-16">
         <div class="text-center">
             <h1 class="text-[#c00000] text-4xl font-bold">OUR SPONSORS</h1>
             <div class="w-48 h-1 mx-auto mt-4 bg-[#365372] rounded-full"></div>
             <p class="text-gray-700 mt-6 text-lg max-w-2xl mx-auto">We are proud to be supported by our amazing sponsors.</p>
         </div>
-        
+
         <div class="mt-12 pb-10">
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8 items-center justify-items-center max-w-6xl mx-auto">
                 <div class="w-48 h-48 text-center">
                     <div class="w-full h-full rounded-full border-4 border-gray-300 shadow-lg overflow-hidden bg-white p-2 hover:shadow-xl transition duration-300">
                         <img class="w-full h-full object-contain" src="images/sp4.jpg" alt="Sponsor 1">
                     </div>
-                </div>        
+                </div>
                 <div class="w-48 h-48 text-center">
                     <div class="w-full h-full rounded-full border-4 border-gray-300 shadow-lg overflow-hidden bg-white p-2 hover:shadow-xl transition duration-300">
                         <img class="w-full h-full object-contain" src="images/sp6.jpg" alt="Sponsor 2">
                     </div>
-                </div>        
+                </div>
                 <div class="w-48 h-48 text-center">
                     <div class="w-full h-full rounded-full border-4 border-gray-300 shadow-lg overflow-hidden bg-black p-2 hover:shadow-xl transition duration-300">
                         <img class="w-full h-full object-contain" src="images/sp1.png" alt="Sponsor 3">
@@ -49,5 +48,5 @@
                 </div>
             </div>
         </div>
-    </div> -->
+    </div>
 </div>

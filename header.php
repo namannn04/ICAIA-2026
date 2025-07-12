9be9a0025c5a9b71a05d20a4179c0009184c23d4
Naman Dadhich <namandadhich15592@gmail.com>
2025-07-12 15:20:51 +0530

    header position and extra box

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
   <div class="flex justify-evenly items-center">
     <img class="w-[150px] mx-auto " src="./images/icaiaLogo.png" alt="">
   </div>
-  <div class='mx-[5%] md:mx-[15%] py-6 my-20 rounded-xl shadow border-2 border-white' style='background-color: rgba(255, 255, 255, 0.6);'>
+  <div class='mx-[5%] md:mx-[15%] py-6 my-10 rounded-xl shadow border-2 border-white' style='background-color: rgba(255, 255, 255, 0.6);'>
     <div class='text-center text-white text-xl md:text-2xl lg:text-3xl font-[700] mx-auto'>
-      <div class="flex items-center justify-between w-[95%] mx-auto">
+      <div class="flex items-center justify-center w-full">
         <div class="flex flex-col items-center">
           <h1 class='z-10 md:tracking-widest tracking-[1px] text-blue-800'>4<sup>th</sup> International Conference on</h1>
           <h1 class="md:tracking-[2px] tracking-[1px] text-blue-800">Artificial Intelligence and Applications (ICAIA 2026)</h1>
@@ -13,11 +13,14 @@
       <h1 class='tracking-[1px] md:tracking-[2.5px] text-[#D3B683] text-black mt-4 font-semibold text-2xl sm:text-xl md:text-2xl lg:text-3xl'>
         Maharaja Surajmal Institute of Technology
       </h1>
-      <div class='text-center text-2xl font-[700] mt-4'>
-        <h1 class='text-[#c00000] text-3xl z-10 tracking-widest mt-3 font-bold text-border event-date text-2xl sm:text-1xl md:text-2xl lg:text-3xl'>
-          19th-20th November 2026
-        </h1>
-      </div>
+    </div>
+  </div>
+
+  <div class='mx-[5%] md:mx-[15%] py-6 my-10 rounded-xl shadow border-2 border-white' style='background-color: rgba(255, 255, 255, 0.6);'>
+    <div class='text-center text-2xl font-[700]'>
+      <h1 class='text-[#c00000] text-3xl z-10 tracking-widest font-bold text-border event-date text-2xl sm:text-1xl md:text-2xl lg:text-3xl'>
+        Conference dates :- 19th-20th November, 2026
+      </h1>
     </div>
   </div>
 </div>
